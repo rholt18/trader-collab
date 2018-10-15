29c77ff11d5c9fb476f02afb537abdca76619543
rholt <rholt@LAPTOP-0MLP7QTR.Home>
2018-10-15 23:37:31 +1030

    Removed Library class

--- src/main/java/com/trader/strategy/AutoTrader.java
+++ src/main/java/com/trader/strategy/AutoTrader.java
@@ -1,12 +1,19 @@
 package com.trader.strategy;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AutoTrader {
+	
+	public static String dateTime() {
+		return SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.LONG).format(new Date());
+	}
 
 	public static void main(String[] args) {
-		System.out.println("AutoTrader Starting");
+		System.out.println(dateTime() + ": AutoTrader Starting");
 		AutoTrader autoTrader = new AutoTrader();
 		autoTrader.execute();
-		System.out.println("AutoTrader Ending");
+		System.out.println(dateTime() + ": AutoTrader Ending");
 	}
 
 	public boolean execute() {
@@ -15,7 +22,7 @@ public class AutoTrader {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return false;
+		return true;
 	}
 
 }
